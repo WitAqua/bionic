a32ab413684b6198d8ae1bb0440f4fb4fe814ac9
Christopher Ferris <cferris@google.com>
2025-05-13 19:10:11 +0000

    Add the running of a trace into libmemory_trace.
    This allows another process to run through a trace quickly.
    
    Test: All benchmarks traces can run and unit tests pass.
    Change-Id: I8831d92de112f3d5c3cd76f726d6beaba44c7a55

--- memory/replay/TraceBenchmark.cpp
+++ memory/replay/TraceBenchmark.cpp
@@ -36,218 +36,9 @@
 #include <benchmark/benchmark.h>
 
 #include <memory_trace/MemoryTrace.h>
+#include <memory_trace/TraceInfo.h>
 
 #include "File.h"
-#include "Utils.h"
-
-struct TraceDataType {
-  memory_trace::Entry* entries = nullptr;
-  size_t num_entries = 0;
-  void** ptrs = nullptr;
-  size_t num_ptrs = 0;
-};
-
-static size_t GetIndex(std::stack<size_t>& free_indices, size_t* max_index) {
-  if (free_indices.empty()) {
-    return (*max_index)++;
-  }
-  size_t index = free_indices.top();
-  free_indices.pop();
-  return index;
-}
-
-static void FreePtrs(TraceDataType* trace_data) {
-  for (size_t i = 0; i < trace_data->num_ptrs; i++) {
-    void* ptr = trace_data->ptrs[i];
-    if (ptr != nullptr) {
-      free(ptr);
-      trace_data->ptrs[i] = nullptr;
-    }
-  }
-}
-
-static void FreeTraceData(TraceDataType* trace_data) {
-  if (trace_data->ptrs == nullptr) {
-    return;
-  }
-
-  munmap(trace_data->ptrs, sizeof(void*) * trace_data->num_ptrs);
-  FreeEntries(trace_data->entries, trace_data->num_entries);
-}
-
-static void GetTraceData(const std::string& filename, TraceDataType* trace_data) {
-  // Only keep last trace encountered cached.
-  static std::string cached_filename;
-  static TraceDataType cached_trace_data;
-  if (cached_filename == filename) {
-    *trace_data = cached_trace_data;
-    return;
-  } else {
-    FreeTraceData(&cached_trace_data);
-  }
-
-  cached_filename = filename;
-  GetUnwindInfo(filename.c_str(), &trace_data->entries, &trace_data->num_entries);
-
-  // This loop will convert the ptr field into an index into the ptrs array.
-  // Creating this index allows the trace run to quickly store or retrieve the
-  // allocation.
-  // For free, the ptr field will be index + one, where a zero represents
-  // a free(nullptr) call.
-  // For realloc, the old_pointer field will be index + one, where a zero
-  // represents a realloc(nullptr, XX).
-  trace_data->num_ptrs = 0;
-  std::stack<size_t> free_indices;
-  std::unordered_map<uint64_t, size_t> ptr_to_index;
-  for (size_t i = 0; i < trace_data->num_entries; i++) {
-    memory_trace::Entry* entry = &trace_data->entries[i];
-    switch (entry->type) {
-      case memory_trace::MALLOC:
-      case memory_trace::CALLOC:
-      case memory_trace::MEMALIGN: {
-        size_t idx = GetIndex(free_indices, &trace_data->num_ptrs);
-        ptr_to_index[entry->ptr] = idx;
-        entry->ptr = idx;
-        break;
-      }
-      case memory_trace::REALLOC: {
-        if (entry->u.old_ptr != 0) {
-          auto idx_entry = ptr_to_index.find(entry->u.old_ptr);
-          if (idx_entry == ptr_to_index.end()) {
-            errx(1, "File Error: Failed to find realloc pointer %" PRIx64, entry->u.old_ptr);
-          }
-          size_t old_pointer_idx = idx_entry->second;
-          free_indices.push(old_pointer_idx);
-          ptr_to_index.erase(idx_entry);
-          entry->u.old_ptr = old_pointer_idx + 1;
-        }
-        size_t idx = GetIndex(free_indices, &trace_data->num_ptrs);
-        ptr_to_index[entry->ptr] = idx;
-        entry->ptr = idx;
-        break;
-      }
-      case memory_trace::FREE:
-        if (entry->ptr != 0) {
-          auto idx_entry = ptr_to_index.find(entry->ptr);
-          if (idx_entry == ptr_to_index.end()) {
-            errx(1, "File Error: Unable to find free pointer %" PRIx64, entry->ptr);
-          }
-          free_indices.push(idx_entry->second);
-          entry->ptr = idx_entry->second + 1;
-          ptr_to_index.erase(idx_entry);
-        }
-        break;
-      case memory_trace::THREAD_DONE:
-      case memory_trace::UNKNOWN:
-        break;
-    }
-  }
-  void* map = mmap(nullptr, sizeof(void*) * trace_data->num_ptrs, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
-  if (map == MAP_FAILED) {
-    err(1, "mmap failed");
-  }
-  trace_data->ptrs = reinterpret_cast<void**>(map);
-
-  cached_trace_data = *trace_data;
-}
-
-static void RunTrace(benchmark::State& state, TraceDataType* trace_data) {
-  int pagesize = getpagesize();
-  uint64_t total_ns = 0;
-  uint64_t start_ns;
-  void** ptrs = trace_data->ptrs;
-  for (size_t i = 0; i < trace_data->num_entries; i++) {
-    void* ptr;
-    const memory_trace::Entry& entry = trace_data->entries[i];
-    switch (entry.type) {
-      case memory_trace::MALLOC:
-        start_ns = Nanotime();
-        ptr = malloc(entry.size);
-        if (ptr == nullptr) {
-          errx(1, "malloc returned nullptr");
-        }
-        MakeAllocationResident(ptr, entry.size, entry.present_bytes, pagesize);
-        total_ns += Nanotime() - start_ns;
-
-        if (ptrs[entry.ptr] != nullptr) {
-          errx(1, "Internal Error: malloc pointer being replaced is not nullptr");
-        }
-        ptrs[entry.ptr] = ptr;
-        break;
-
-      case memory_trace::CALLOC:
-        start_ns = Nanotime();
-        ptr = calloc(entry.u.n_elements, entry.size);
-        if (ptr == nullptr) {
-          errx(1, "calloc returned nullptr");
-        }
-        MakeAllocationResident(ptr, entry.size, entry.present_bytes, pagesize);
-        total_ns += Nanotime() - start_ns;
-
-        if (ptrs[entry.ptr] != nullptr) {
-          errx(1, "Internal Error: calloc pointer being replaced is not nullptr");
-        }
-        ptrs[entry.ptr] = ptr;
-        break;
-
-      case memory_trace::MEMALIGN:
-        start_ns = Nanotime();
-        ptr = memalign(entry.u.align, entry.size);
-        if (ptr == nullptr) {
-          errx(1, "memalign returned nullptr");
-        }
-        MakeAllocationResident(ptr, entry.size, entry.present_bytes, pagesize);
-        total_ns += Nanotime() - start_ns;
-
-        if (ptrs[entry.ptr] != nullptr) {
-          errx(1, "Internal Error: memalign pointer being replaced is not nullptr");
-        }
-        ptrs[entry.ptr] = ptr;
-        break;
-
-      case memory_trace::REALLOC:
-        start_ns = Nanotime();
-        if (entry.u.old_ptr == 0) {
-          ptr = realloc(nullptr, entry.size);
-        } else {
-          ptr = realloc(ptrs[entry.u.old_ptr - 1], entry.size);
-          ptrs[entry.u.old_ptr - 1] = nullptr;
-        }
-        if (entry.size > 0) {
-          if (ptr == nullptr) {
-            errx(1, "realloc returned nullptr");
-          }
-          MakeAllocationResident(ptr, entry.size, entry.present_bytes, pagesize);
-        }
-        total_ns += Nanotime() - start_ns;
-
-        if (ptrs[entry.ptr] != nullptr) {
-          errx(1, "Internal Error: realloc pointer being replaced is not nullptr");
-        }
-        ptrs[entry.ptr] = ptr;
-        break;
-
-      case memory_trace::FREE:
-        if (entry.ptr != 0) {
-          ptr = ptrs[entry.ptr - 1];
-          ptrs[entry.ptr - 1] = nullptr;
-        } else {
-          ptr = nullptr;
-        }
-        start_ns = Nanotime();
-        free(ptr);
-        total_ns += Nanotime() - start_ns;
-        break;
-
-      case memory_trace::THREAD_DONE:
-      case memory_trace::UNKNOWN:
-        break;
-    }
-  }
-  state.SetIterationTime(total_ns / double(1000000000.0));
-
-  FreePtrs(trace_data);
-}
 
 // Run a trace as if all of the allocations occurred in a single thread.
 // This is not completely realistic, but it is a possible worst case that
@@ -263,11 +54,15 @@ static void BenchmarkTrace(benchmark::State& state, const char* filename,
 #endif
   std::string full_filename(android::base::GetExecutableDirectory() + "/traces/" + filename);
 
-  TraceDataType trace_data;
-  GetTraceData(full_filename, &trace_data);
+  static TraceInfo trace;
+  if (full_filename != trace.filename()) {
+    trace.Clear();
+    trace.Init(full_filename);
+  }
 
   for (auto _ : state) {
-    RunTrace(state, &trace_data);
+    uint64_t total_ns = trace.RunSingleThread();
+    state.SetIterationTime(total_ns / double(1000000000.0));
   }
 
   // Don't free the trace_data, it is cached. The last set of trace data
