16bb2529a3a254fb1cd96a2476f0652a50238d6d
Elliott Hughes <enh@google.com>
2025-09-15 10:02:10 -0700

    stdio: move __srefill() into stdio.cpp.
    Only cosmetic/whitespace changes.
    
    Change-Id: Ibbbe137147ce6eb2ce2a01351cab97118348e14f

--- libc/stdio/stdio.cpp
+++ libc/stdio/stdio.cpp
@@ -236,6 +236,86 @@ extern "C" __LIBC_HIDDEN__ void __libc_stdio_cleanup(void) {
   _fwalk(__sflush);
 }
 
+static int lflush(FILE* fp) {
+  if ((fp->_flags & (__SLBF|__SWR)) == (__SLBF|__SWR)) {
+    return __sflush_locked(fp); /* ignored... */
+  }
+  return 0;
+}
+
+/*
+ * Refill a stdio buffer.
+ * Return EOF on eof or error, 0 otherwise.
+ */
+int __srefill(FILE* fp) {
+  fp->_r = 0; /* largely a convenience for callers */
+
+#if !defined(__BIONIC__)
+  /* SysV does not make this test; take it out for compatibility */
+  if (fp->_flags & __SEOF) return (EOF);
+#endif
+
+  /* if not already reading, have to be reading and writing */
+  if ((fp->_flags & __SRD) == 0) {
+    if ((fp->_flags & __SRW) == 0) {
+      errno = EBADF;
+      fp->_flags |= __SERR;
+      return EOF;
+    }
+    /* switch to reading */
+    if (fp->_flags & __SWR) {
+      if (__sflush(fp)) return EOF;
+      fp->_flags &= ~__SWR;
+      fp->_w = 0;
+      fp->_lbfsize = 0;
+    }
+    fp->_flags |= __SRD;
+  } else {
+    /*
+     * We were reading.  If there is an ungetc buffer,
+     * we must have been reading from that.  Drop it,
+     * restoring the previous buffer (if any).  If there
+     * is anything in that buffer, return.
+     */
+    if (HASUB(fp)) {
+      FREEUB(fp);
+      if ((fp->_r = fp->_ur) != 0) {
+        fp->_p = fp->_up;
+        return 0;
+      }
+    }
+  }
+
+  if (fp->_bf._base == NULL) __smakebuf(fp);
+
+  /*
+   * Before reading from a line buffered or unbuffered file,
+   * flush all line buffered output files, per the ANSI C
+   * standard.
+   */
+  if (fp->_flags & (__SLBF|__SNBF)) {
+    /* Ignore this file in _fwalk to avoid potential deadlock. */
+    fp->_flags |= __SIGN;
+    (void) _fwalk(lflush);
+    fp->_flags &= ~__SIGN;
+
+    /* Now flush this file without locking it. */
+    if ((fp->_flags & (__SLBF|__SWR)) == (__SLBF|__SWR)) __sflush(fp);
+  }
+  fp->_p = fp->_bf._base;
+  fp->_r = (*fp->_read)(fp->_cookie, reinterpret_cast<char*>(fp->_p), fp->_bf._size);
+  if (fp->_r <= 0) {
+    if (fp->_r == 0) {
+      fp->_flags |= __SEOF;
+    } else {
+      fp->_r = 0;
+      fp->_flags |= __SERR;
+    }
+    return EOF;
+  }
+  return 0;
+}
+
 /*
  * Allocate a file buffer, or switch to unbuffered I/O.
  * Per the ANSI C standard, ALL tty devices default to line buffered.
