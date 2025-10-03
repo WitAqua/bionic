9e5870240135b2a7960614cc8743d5c153b14740
Elliott Hughes <enh@google.com>
2025-10-03 18:30:28 +0000

    <string.h>: write our own strcspn()/strspn().
    The massive speedup from using our optimized arm64 strchrnul() here
    for the most common case suggests there's the potential for these to
    be significantly faster for the other cases if we had some vector/simd
    code here.
    
    In the meantime, this is faster for all inputs than the nested loop
    traditional BSD code (which does actually do pretty well for a single
    delimiter, at the expense of poor performance with multiple delimiters)
    and faster than the obvious bitset code. Though somewhere hboehm is
    muttering about me trashing the data cache for my microbenchmark, I
    think it's probably justified here because (a) it's the stack, (b) it's
    only 256 bytes (and you're probably only hitting the first 64), and (c)
    you're calling these functions because you're doing string processing,
    and the input -- rather than the delimiters -- is where your data cache
    is going.
    
    Bug: http://b/437059814
    Change-Id: Ifa441e2818422c32d671b5b15827748e8f876e79

--- libc/bionic/string.cpp
+++ libc/bionic/string.cpp
@@ -29,6 +29,49 @@
 #include <string.h>
 #include <xlocale.h>
 
+// Benchmarking shows that bool[] works better than a bitset,
+// and 256 bytes of stack (the latter half of which is never used in practice)
+// doesn't seem unreasonable.
+static_assert(sizeof(bool) == 1);
+static inline void init_delimiter_set(bool* set, const char* delims) {
+  for (const uint8_t* d = reinterpret_cast<const uint8_t*>(delims); *d; ++d) {
+    set[*d] = true;
+  }
+}
+
+__attribute__((__flatten__))
+size_t strspn(const char* ss, const char* delims) {
+  const uint8_t* s = reinterpret_cast<const uint8_t*>(ss);
+  const uint8_t* p = s;
+  if (delims[0] == '\0') return 0;
+  if (delims[1] == '\0') {
+    // The common case is a single delimiter, where the set is worse.
+    while (*p == delims[0]) ++p;
+  } else {
+    bool set[256] = {};
+    init_delimiter_set(set, delims);
+    while (set[*p]) ++p;
+  }
+  return p - s;
+}
+
+__attribute__((__flatten__))
+size_t strcspn(const char* ss, const char* delims) {
+  const uint8_t* s = reinterpret_cast<const uint8_t*>(ss);
+  const uint8_t* p = s;
+  if (delims[0] == '\0' || delims[1] == '\0') {
+    // The common case is a single delimiter, where the set is far worse.
+    // On arm64 strchrnul() is faster than open coding even for small distances,
+    // and orders of magnitude better for large distances.
+    return strchrnul(ss, delims[0]) - ss;
+  } else {
+    bool set[256] = {};
+    init_delimiter_set(set, delims);
+    while (*p && !set[*p]) ++p;
+  }
+  return p - s;
+}
+
 int strcoll(const char* lhs, const char* rhs) {
   return strcmp(lhs, rhs);
 }
