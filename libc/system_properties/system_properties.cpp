f68f809dbc8b1b53a8a48cc88a8ca37de893a665
pdxt <pdxt@google.com>
2025-12-03 14:40:27 +0000

    Fix appcompat override mechanism in system_properties
    We updated the mechanism by simplifying the Reload, and updating Find to
    use appcompat_override prefixed system properties if this flow is
    triggered.
    
    Bug: 427143830
    Test: Run impacted apps
    Change-Id: Ic6cddc0a18ab61ef5f894e6f0db274ee8ab9746f

--- libc/system_properties/system_properties.cpp
+++ libc/system_properties/system_properties.cpp
@@ -137,12 +137,10 @@ bool SystemProperties::AreaInit(const char* filename, bool* fsetxattr_failed,
   return true;
 }
 
-bool SystemProperties::Reload(bool load_default_path) {
-  if (!initialized_) {
-    return true;
-  }
-
-  return InitContexts(load_default_path);
+bool SystemProperties::EnableOverrides() {
+  CHECK(initialized_);
+  use_appcompat_override_ = true;
+  return true;
 }
 
 uint32_t SystemProperties::AreaSerial() {
@@ -162,6 +160,23 @@ uint32_t SystemProperties::AreaSerial() {
 const prop_info* SystemProperties::Find(const char* name) {
   if (!initialized_) {
     return nullptr;
+  }
+
+  // if appcompat override is enabled, we first try finding APPCOMPAT_PREFIXed system
+  // property
+  if (use_appcompat_override_) {
+    const size_t totalLength = strlen(APPCOMPAT_PREFIX) + strlen(name) + 1;
+    char overrideName[totalLength];
+
+    snprintf(overrideName, totalLength, "%s%s", APPCOMPAT_PREFIX, name);
+
+    prop_area* pa = contexts_->GetPropAreaForName(overrideName);
+    if (pa) {
+      const prop_info* pi = pa->find(overrideName);
+      if (pi) {
+        return pi;
+      }
+    }
   }
 
   prop_area* pa = contexts_->GetPropAreaForName(name);
