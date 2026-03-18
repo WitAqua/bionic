8c9ce1f5f6ac4e232871c9049814130f62bede7c
Elliott Hughes <enh@google.com>
2026-03-18 17:52:07 +0000

    putenv()/setenv(): deduplicate environ-resizing code.
    Initially I thought we had to do this to be able to add locking, but
    at this point my plan is to [continue to] never free "name=value"
    strings, and to [continue to] free (via reallocarray()) old environ
    arrays. The reasoning being that we can't protect you from yourself
    anyway if you're directly messing around with environ on one thread and
    calling environ-traversing functions on another --- all we can do is
    make [more reasonable] cases such as concurrent getenv()+setenv() calls.
    
    But that's no excuse not to clean up this code while we're here.
    
    Change-Id: I6f9d512e1ca2684fec1c84ee044e00ca29e00c83

--- libc/bionic/env.cpp
+++ libc/bionic/env.cpp
@@ -32,8 +32,6 @@
 #include <string.h>
 #include <unistd.h>
 
-static char** lastenv;        /* last value of environ */
-
 // Returns a pointer to the value associated with name,
 // or nullptr if not found.
 // Starts searching environ from *offset,
@@ -63,13 +61,43 @@ char* getenv(const char* name) {
   return __findenv(name, name_length, &offset);
 }
 
-static size_t __current_env_size() {
-  size_t result = 0;
+static bool __add_new_environ_slot(size_t* offset) {
+  size_t old_size = 0;
   if (environ != nullptr) {
     char** p = environ;
-    for (; *p != nullptr; ++p) ++result;
+    for (; *p != nullptr; ++p) ++old_size;
   }
-  return result;
+
+  // Keep track of the most recent environ allocation we did.
+  // We can't just pass environ to reallocarray() because that might
+  // have been replaced with non-heap-allocated memory. Supporting that
+  // is probably a mistake, but it's our (and everyone else's) historical
+  // behavior.
+  static char** old_env = nullptr;
+
+  char** new_env = static_cast<char**>(reallocarray(old_env, old_size + 2, sizeof(char*)));
+  if (!new_env) return false;
+  if (old_env != environ && environ != nullptr) {
+    memcpy(new_env, environ, old_size * sizeof(char*));
+  }
+  environ = old_env = new_env;
+  environ[old_size] = environ[old_size + 1] = nullptr;
+  *offset = old_size;
+  return true;
+}
+
+template <typename F>
+static int __update_environ(const char* name, size_t name_length, bool rewrite, F string_maker) {
+  size_t offset = 0;
+  if (__findenv(name, name_length, &offset) != nullptr) {
+    if (!rewrite) return 0;
+  } else {
+    if (!__add_new_environ_slot(&offset)) return -1;
+  }
+
+  char* str = string_maker();
+  environ[offset] = str;
+  return str ? 0 : -1;
 }
 
 int putenv(char* str) {
@@ -83,25 +111,7 @@ int putenv(char* str) {
     return -1;
   }
 
-  size_t offset = 0;
-  if (__findenv(str, name_length, &offset) != nullptr) {
-    environ[offset++] = str;
-    return 0;
-  }
-
-  /* create new slot for string */
-  size_t cnt = __current_env_size();
-  char** p = static_cast<char**>(reallocarray(lastenv, cnt + 2, sizeof(char*)));
-  if (!p) {
-    return -1;
-  }
-  if (lastenv != environ && environ != nullptr) {
-    memcpy(p, environ, cnt * sizeof(char*));
-  }
-  lastenv = environ = p;
-  environ[cnt] = str;
-  environ[cnt + 1] = nullptr;
-  return 0;
+  return __update_environ(str, name_length, true, [str]() { return str; });
 }
 
 int setenv(const char* name, const char* value, int rewrite) {
@@ -113,31 +123,16 @@ int setenv(const char* name, const char* value, int rewrite) {
     return -1;
   }
 
-  size_t offset = 0;
-  if (__findenv(name, name_length, &offset) != nullptr) {
-    if (!rewrite) return 0;
-  } else {          /* create new slot */
-    size_t cnt = __current_env_size();
-    char** p = static_cast<char**>(reallocarray(lastenv, cnt + 2, sizeof(char*)));
-    if (!p) {
-      return -1;
+  auto string_maker = [name, name_length, value]() {
+    // Turn "name" and "value" into "name=value" for insertion into environ.
+    size_t value_length = strlen(value);
+    char* str = static_cast<char*>(malloc(name_length + 1 + value_length + 1));
+    if (str != nullptr) {
+      mempcpy(mempcpy(mempcpy(str, name, name_length), "=", 1), value, value_length + 1);
     }
-    if (lastenv != environ && environ != nullptr) {
-      memcpy(p, environ, cnt * sizeof(char*));
-    }
-    lastenv = environ = p;
-    offset = cnt;
-    environ[cnt + 1] = nullptr;
-  }
-
-  // Turn "name" and "value" into "name=value" for insertion into environ.
-  size_t value_length = strlen(value);
-  char* str = static_cast<char*>(malloc(name_length + 1 + value_length + 1));
-  if (str == nullptr) return -1;
-  mempcpy(mempcpy(mempcpy(str, name, name_length), "=", 1), value, value_length + 1);
-
-  environ[offset] = str;
-  return 0;
+    return str;
+  };
+  return __update_environ(name, name_length, rewrite, string_maker);
 }
 
 int unsetenv(const char* name) {
