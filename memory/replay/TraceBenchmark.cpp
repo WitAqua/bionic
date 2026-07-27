jbeich/platform_system_extras

Refactor code with single trace parser.
This is the first step towards adding a new way to benchmark the
traces. This new way would be a way to add all threads running at once.

Also to try and make it possible to do the faster runs that jmgao had
done a while ago.

Modified the code so that it's possible for memory_replay to play
the zip file, no need to unzip the trace files.

Add a single parser for the trace files.

Remove as many possible allocation points in the system. This allows
the traces to be run as pure as possible.

Added new unit tests to cover new code.

Bug: 137795072

Test: All unit tests pass.
Test: Ran select traces using the old version and the new version.
Test: Both get nearlyt he same RSS, but slightly different VA space
Test: due to the new version avoiding allocations at all costs.
Change-Id: I8e8a28436f23b09eba57772c62bb0b866d874437

memory_replay/TraceBenchmark.cpp
@@ -22,11 +22,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
 #include <unistd.h>
 
 #include <algorithm>
-#include <memory>
 #include <stack>
 #include <string>
 #include <unordered_map>
@@ -35,62 +33,40 @@
 #include <android-base/file.h>
 #include <android-base/strings.h>
 #include <benchmark/benchmark.h>
-#include <ziparchive/zip_archive.h>
 
-enum AllocEnum : uint8_t {
-  MALLOC = 0,   // arg2 not used
-  CALLOC,       // size = item_count, arg2 = item_size
-  MEMALIGN,     // arg2 = alignment
-  REALLOC,      // if arg2 = 0, ptr arg is nullptr, else arg2 = old pointer index + 1
-  FREE,         // size not used, arg2 not used
-};
+#include "Alloc.h"
+#include "Utils.h"
+#include "Zip.h"
 
-struct MallocEntry {
-  MallocEntry(AllocEnum type, size_t idx, size_t size, size_t arg2)
-      : type(type), idx(idx), size(size), arg2(arg2) {}
+struct TraceAllocEntry {
+  TraceAllocEntry(AllocEnum type, size_t idx, size_t size, size_t anon)
+      : type(type), idx(idx), size(size) {
+    u.anon = anon;
+  }
   AllocEnum type;
   size_t idx;
   size_t size;
-  size_t arg2;
+  union {
+    size_t anon = 0;
+    size_t old_idx;
+    size_t align;
+    size_t n_elements;
+  } u;
 };
 
-static std::string GetZipContents(const char* filename) {
-  ZipArchiveHandle archive;
-  if (OpenArchive(filename, &archive) != 0) {
-    return "";
-  }
-
-  std::string contents;
-  void* cookie;
-  if (StartIteration(archive, &cookie) == 0) {
-    ZipEntry entry;
-    std::string name;
-    if (Next(cookie, &entry, &name) == 0) {
-      contents.resize(entry.uncompressed_length);
-      if (ExtractToMemory(archive, &entry, reinterpret_cast<uint8_t*>(contents.data()),
-                          entry.uncompressed_length) != 0) {
-        contents = "";
-      }
-    }
-  }
-
-  CloseArchive(archive);
-  return contents;
-}
-
-static size_t GetIndex(std::stack<size_t>& indices, size_t* max_index) {
+static size_t GetIndex(std::stack<uint64_t>& indices, size_t* max_index) {
   if (indices.empty()) {
     return (*max_index)++;
   }
-  size_t index = indices.top();
+  uint64_t index = indices.top();
   indices.pop();
   return index;
 }
 
-static std::vector<MallocEntry>* GetTraceData(const char* filename, size_t* max_ptrs) {
+static std::vector<TraceAllocEntry>* GetTraceData(const char* filename, size_t* max_ptrs) {
   // Only keep last trace encountered cached.
   static std::string cached_filename;
-  static std::vector<MallocEntry> cached_entries;
+  static std::vector<TraceAllocEntry> cached_entries;
   static size_t cached_max_ptrs;
 
   if (cached_filename == filename) {
@@ -102,102 +78,72 @@ static std::vector<MallocEntry>* GetTraceData(const char* filename, size_t* max_
   cached_max_ptrs = 0;
   cached_filename = filename;
 
-  std::string content(GetZipContents(filename));
+  std::string content(ZipGetContents(filename));
   if (content.empty()) {
     errx(1, "Internal Error: Empty zip file %s", filename);
   }
   std::vector<std::string> lines(android::base::Split(content, "\n"));
 
   *max_ptrs = 0;
-  std::stack<size_t> free_indices;
-  std::unordered_map<uintptr_t, size_t> indices;
-  std::vector<MallocEntry>* entries = &cached_entries;
+  std::stack<uint64_t> free_indices;
+  std::unordered_map<uint64_t, uint64_t> indices;
+  std::vector<TraceAllocEntry>* entries = &cached_entries;
   for (const std::string& line : lines) {
     if (line.empty()) {
       continue;
     }
-    pid_t tid;
-    int line_pos = 0;
-    char name[128];
-    uintptr_t pointer;
-    // All lines have this format:
-    //   TID: ALLOCATION_TYPE POINTER
-    // where
-    //   TID is the thread id of the thread doing the operation.
-    //   ALLOCATION_TYPE is one of malloc, calloc, memalign, realloc, free, thread_done
-    //   POINTER is the hex value of the actual pointer
-    if (sscanf(line.c_str(), "%d: %127s %" SCNxPTR " %n", &tid, name, &pointer, &line_pos) != 3) {
-      errx(1, "Internal Error: Failed to process %s", line.c_str());
-    }
-    const char* line_end = &line[line_pos];
-    std::string type(name);
-    if (type == "malloc") {
-      // Format:
-      //   TID: malloc POINTER SIZE_OF_ALLOCATION
-      size_t size;
-      if (sscanf(line_end, "%zu", &size) != 1) {
-        errx(1, "Internal Error: Failed to read malloc data %s", line.c_str());
+    AllocEntry entry;
+    AllocGetData(line, &entry);
+
+    switch (entry.type) {
+      case MALLOC: {
+        size_t idx = GetIndex(free_indices, max_ptrs);
+        indices[entry.ptr] = idx;
+        entries->emplace_back(MALLOC, idx, entry.size, 0);
+        break;
       }
-      size_t idx = GetIndex(free_indices, max_ptrs);
-      indices[pointer] = idx;
-      entries->emplace_back(MALLOC, idx, size, 0);
-    } else if (type == "free") {
-      // Format:
-      //   TID: free POINTER
-      if (pointer != 0) {
-        auto entry = indices.find(pointer);
-        if (entry == indices.end()) {
-          errx(1, "Internal Error: Unable to find free pointer %" PRIuPTR, pointer);
+      case CALLOC: {
+        size_t idx = GetIndex(free_indices, max_ptrs);
+        indices[entry.ptr] = idx;
+        entries->emplace_back(CALLOC, idx, entry.u.n_elements, entry.size);
+        break;
+      }
+      case MEMALIGN: {
+        size_t idx = GetIndex(free_indices, max_ptrs);
+        indices[entry.ptr] = idx;
+        entries->emplace_back(MEMALIGN, idx, entry.size, entry.u.align);
+        break;
+      }
+      case REALLOC: {
+        size_t old_pointer_idx = 0;
+        if (entry.u.old_ptr != 0) {
+          auto idx_entry = indices.find(entry.u.old_ptr);
+          if (idx_entry == indices.end()) {
+            errx(1, "File Error: Failed to find realloc pointer %" PRIu64, entry.u.old_ptr);
+          }
+          old_pointer_idx = idx_entry->second;
+          free_indices.push(old_pointer_idx);
         }
-        free_indices.push(entry->second);
-        entries->emplace_back(FREE, entry->second + 1, 0, 0);
-      } else {
-        entries->emplace_back(FREE, 0, 0, 0);
+        size_t idx = GetIndex(free_indices, max_ptrs);
+        indices[entry.ptr] = idx;
+        entries->emplace_back(REALLOC, idx, entry.size, old_pointer_idx + 1);
+        break;
       }
-    } else if (type == "calloc") {
-      // Format:
-      //   TID: calloc POINTER ITEM_SIZE ITEM_COUNT
-      size_t n_elements;
-      size_t size;
-      if (sscanf(line_end, "%zu %zu", &n_elements, &size) != 2) {
-        errx(1, "Internal Error: Failed to read calloc data %s", line.c_str());
-      }
-      size_t idx = GetIndex(free_indices, max_ptrs);
-      indices[pointer] = idx;
-      entries->emplace_back(CALLOC, idx, size, n_elements);
-    } else if (type == "realloc") {
-      // Format:
-      //   TID: calloc POINTER NEW_SIZE OLD_POINTER
-      uintptr_t old_pointer;
-      size_t size;
-      if (sscanf(line_end, "%" SCNxPTR " %zu", &old_pointer, &size) != 2) {
-        errx(1, "Internal Error: Failed to read realloc data %s", line.c_str());
-      }
-      size_t old_pointer_idx = 0;
-      if (old_pointer != 0) {
-        auto entry = indices.find(old_pointer);
-        if (entry == indices.end()) {
-          errx(1, "Internal Error: Failed to find realloc pointer %" PRIuPTR, old_pointer);
+      case FREE:
+        if (entry.ptr != 0) {
+          auto idx_entry = indices.find(entry.ptr);
+          if (idx_entry == indices.end()) {
+            errx(1, "File Error: Unable to find free pointer %" PRIu64, entry.ptr);
+          }
+          free_indices.push(idx_entry->second);
+          entries->emplace_back(FREE, idx_entry->second + 1, 0, 0);
+        } else {
+          entries->emplace_back(FREE, 0, 0, 0);
         }
-        old_pointer_idx = entry->second;
-        free_indices.push(old_pointer_idx);
-      }
-      size_t idx = GetIndex(free_indices, max_ptrs);
-      indices[pointer] = idx;
-      entries->emplace_back(REALLOC, idx, size, old_pointer_idx + 1);
-    } else if (type == "memalign") {
-      // Format:
-      //   TID: memalign POINTER SIZE ALIGNMENT
-      size_t align;
-      size_t size;
-      if (sscanf(line_end, "%zu %zu", &align, &size) != 2) {
-        errx(1, "Internal Error: Failed to read memalign data %s", line.c_str());
-      }
-      size_t idx = GetIndex(free_indices, max_ptrs);
-      indices[pointer] = idx;
-      entries->emplace_back(MEMALIGN, idx, size, align);
-    } else if (type != "thread_done") {
-      errx(1, "Internal Error: Unknown type %s", line.c_str());
+        break;
+      case THREAD_DONE:
+        // Ignore these.
+        break;
     }
   }
 
@@ -205,21 +151,8 @@ static std::vector<MallocEntry>* GetTraceData(const char* filename, size_t* max_
   return entries;
 }
 
-static __always_inline uint64_t Nanotime() {
-  struct timespec t;
-  t.tv_sec = t.tv_nsec = 0;
-  clock_gettime(CLOCK_MONOTONIC, &t);
-  return static_cast<uint64_t>(t.tv_sec) * 1000000000LL + t.tv_nsec;
-}
-
-static __always_inline void MakeAllocationResident(void* ptr, size_t nbytes, int pagesize) {
-  uint8_t* data = reinterpret_cast<uint8_t*>(ptr);
-  for (size_t i = 0; i < nbytes; i += pagesize) {
-    data[i] = 1;
-  }
-}
-
-static void RunTrace(benchmark::State& state, std::vector<MallocEntry>& entries, size_t max_ptrs) {
+static void RunTrace(benchmark::State& state, std::vector<TraceAllocEntry>& entries,
+                     size_t max_ptrs) {
   std::vector<void*> ptrs(max_ptrs, nullptr);
 
   int pagesize = getpagesize();
@@ -245,7 +178,7 @@ static void RunTrace(benchmark::State& state, std::vector<MallocEntry>& entries,
 
       case CALLOC:
         start_ns = Nanotime();
-        ptr = calloc(entry.arg2, entry.size);
+        ptr = calloc(entry.u.n_elements, entry.size);
         if (ptr == nullptr) {
           errx(1, "calloc returned nullptr");
         }
@@ -260,7 +193,7 @@ static void RunTrace(benchmark::State& state, std::vector<MallocEntry>& entries,
 
       case MEMALIGN:
         start_ns = Nanotime();
-        ptr = memalign(entry.arg2, entry.size);
+        ptr = memalign(entry.u.align, entry.size);
         if (ptr == nullptr) {
           errx(1, "memalign returned nullptr");
         }
@@ -275,11 +208,11 @@ static void RunTrace(benchmark::State& state, std::vector<MallocEntry>& entries,
 
       case REALLOC:
         start_ns = Nanotime();
-        if (entry.arg2 == 0) {
+        if (entry.u.old_idx == 0) {
           ptr = realloc(nullptr, entry.size);
         } else {
-          ptr = realloc(ptrs[entry.arg2 - 1], entry.size);
-          ptrs[entry.arg2 - 1] = nullptr;
+          ptr = realloc(ptrs[entry.u.old_idx - 1], entry.size);
+          ptrs[entry.u.old_idx - 1] = nullptr;
         }
         if (entry.size > 0) {
           if (ptr == nullptr) {
@@ -306,6 +239,9 @@ static void RunTrace(benchmark::State& state, std::vector<MallocEntry>& entries,
         free(ptr);
         total_ns += Nanotime() - start_ns;
         break;
+
+      case THREAD_DONE:
+        break;
     }
   }
   state.SetIterationTime(total_ns / double(1000000000.0));
@@ -319,7 +255,7 @@ static void RunTrace(benchmark::State& state, std::vector<MallocEntry>& entries,
 static void BenchmarkTrace(benchmark::State& state, const char* filename) {
   std::string full_filename(android::base::GetExecutableDirectory() + "/traces/" + filename);
   size_t max_ptrs;
-  std::vector<MallocEntry>* entries = GetTraceData(full_filename.c_str(), &max_ptrs);
+  std::vector<TraceAllocEntry>* entries = GetTraceData(full_filename.c_str(), &max_ptrs);
   if (entries == nullptr) {
     errx(1, "ERROR: Failed to get trace data for %s.", full_filename.c_str());
   }

WitAqua/bionic

Test: Ran the trace benchmarks and verified that the old version and the
Test: new version produce nearly the same numbers.

memory/replay/TraceBenchmark.cpp
@@ -22,11 +22,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
 #include <unistd.h>
 
 #include <algorithm>
-#include <memory>
 #include <stack>
 #include <string>
 #include <unordered_map>
@@ -35,62 +33,39 @@
 #include <android-base/file.h>
 #include <android-base/strings.h>
 #include <benchmark/benchmark.h>
-#include <ziparchive/zip_archive.h>
 
-enum AllocEnum : uint8_t {
-  MALLOC = 0,   // arg2 not used
-  CALLOC,       // size = item_count, arg2 = item_size
-  MEMALIGN,     // arg2 = alignment
-  REALLOC,      // if arg2 = 0, ptr arg is nullptr, else arg2 = old pointer index + 1
-  FREE,         // size not used, arg2 not used
-};
+#include "Alloc.h"
+#include "Utils.h"
+#include "Zip.h"
 
-struct MallocEntry {
-  MallocEntry(AllocEnum type, size_t idx, size_t size, size_t arg2)
-      : type(type), idx(idx), size(size), arg2(arg2) {}
+struct TraceAllocEntry {
+  TraceAllocEntry(AllocEnum type, size_t idx, size_t size, size_t last_arg)
+      : type(type), idx(idx), size(size) {
+    u.old_idx = last_arg;
+  }
   AllocEnum type;
   size_t idx;
   size_t size;
-  size_t arg2;
+  union {
+    size_t old_idx = 0;
+    size_t align;
+    size_t n_elements;
+  } u;
 };
 
-static std::string GetZipContents(const char* filename) {
-  ZipArchiveHandle archive;
-  if (OpenArchive(filename, &archive) != 0) {
-    return "";
-  }
-
-  std::string contents;
-  void* cookie;
-  if (StartIteration(archive, &cookie) == 0) {
-    ZipEntry entry;
-    std::string name;
-    if (Next(cookie, &entry, &name) == 0) {
-      contents.resize(entry.uncompressed_length);
-      if (ExtractToMemory(archive, &entry, reinterpret_cast<uint8_t*>(contents.data()),
-                          entry.uncompressed_length) != 0) {
-        contents = "";
-      }
-    }
-  }
-
-  CloseArchive(archive);
-  return contents;
-}
-
-static size_t GetIndex(std::stack<size_t>& indices, size_t* max_index) {
-  if (indices.empty()) {
+static size_t GetIndex(std::stack<size_t>& free_indices, size_t* max_index) {
+  if (free_indices.empty()) {
     return (*max_index)++;
   }
-  size_t index = indices.top();
-  indices.pop();
+  size_t index = free_indices.top();
+  free_indices.pop();
   return index;
 }
 
-static std::vector<MallocEntry>* GetTraceData(const char* filename, size_t* max_ptrs) {
+static std::vector<TraceAllocEntry>* GetTraceData(const char* filename, size_t* max_ptrs) {
   // Only keep last trace encountered cached.
   static std::string cached_filename;
-  static std::vector<MallocEntry> cached_entries;
+  static std::vector<TraceAllocEntry> cached_entries;
   static size_t cached_max_ptrs;
 
   if (cached_filename == filename) {
@@ -102,7 +77,7 @@ static std::vector<MallocEntry>* GetTraceData(const char* filename, size_t* max_
   cached_max_ptrs = 0;
   cached_filename = filename;
 
-  std::string content(GetZipContents(filename));
+  std::string content(ZipGetContents(filename));
   if (content.empty()) {
     errx(1, "Internal Error: Empty zip file %s", filename);
   }
@@ -110,94 +85,64 @@ static std::vector<MallocEntry>* GetTraceData(const char* filename, size_t* max_
 
   *max_ptrs = 0;
   std::stack<size_t> free_indices;
-  std::unordered_map<uintptr_t, size_t> indices;
-  std::vector<MallocEntry>* entries = &cached_entries;
+  std::unordered_map<uint64_t, size_t> ptr_to_index;
+  std::vector<TraceAllocEntry>* entries = &cached_entries;
   for (const std::string& line : lines) {
     if (line.empty()) {
       continue;
     }
-    pid_t tid;
-    int line_pos = 0;
-    char name[128];
-    uintptr_t pointer;
-    // All lines have this format:
-    //   TID: ALLOCATION_TYPE POINTER
-    // where
-    //   TID is the thread id of the thread doing the operation.
-    //   ALLOCATION_TYPE is one of malloc, calloc, memalign, realloc, free, thread_done
-    //   POINTER is the hex value of the actual pointer
-    if (sscanf(line.c_str(), "%d: %127s %" SCNxPTR " %n", &tid, name, &pointer, &line_pos) != 3) {
-      errx(1, "Internal Error: Failed to process %s", line.c_str());
-    }
-    const char* line_end = &line[line_pos];
-    std::string type(name);
-    if (type == "malloc") {
-      // Format:
-      //   TID: malloc POINTER SIZE_OF_ALLOCATION
-      size_t size;
-      if (sscanf(line_end, "%zu", &size) != 1) {
-        errx(1, "Internal Error: Failed to read malloc data %s", line.c_str());
+    AllocEntry entry;
+    AllocGetData(line, &entry);
+
+    switch (entry.type) {
+      case MALLOC: {
+        size_t idx = GetIndex(free_indices, max_ptrs);
+        ptr_to_index[entry.ptr] = idx;
+        entries->emplace_back(MALLOC, idx, entry.size, 0);
+        break;
       }
-      size_t idx = GetIndex(free_indices, max_ptrs);
-      indices[pointer] = idx;
-      entries->emplace_back(MALLOC, idx, size, 0);
-    } else if (type == "free") {
-      // Format:
-      //   TID: free POINTER
-      if (pointer != 0) {
-        auto entry = indices.find(pointer);
-        if (entry == indices.end()) {
-          errx(1, "Internal Error: Unable to find free pointer %" PRIuPTR, pointer);
+      case CALLOC: {
+        size_t idx = GetIndex(free_indices, max_ptrs);
+        ptr_to_index[entry.ptr] = idx;
+        entries->emplace_back(CALLOC, idx, entry.u.n_elements, entry.size);
+        break;
+      }
+      case MEMALIGN: {
+        size_t idx = GetIndex(free_indices, max_ptrs);
+        ptr_to_index[entry.ptr] = idx;
+        entries->emplace_back(MEMALIGN, idx, entry.size, entry.u.align);
+        break;
+      }
+      case REALLOC: {
+        size_t old_pointer_idx = 0;
+        if (entry.u.old_ptr != 0) {
+          auto idx_entry = ptr_to_index.find(entry.u.old_ptr);
+          if (idx_entry == ptr_to_index.end()) {
+            errx(1, "File Error: Failed to find realloc pointer %" PRIu64, entry.u.old_ptr);
+          }
+          old_pointer_idx = idx_entry->second;
+          free_indices.push(old_pointer_idx);
         }
-        free_indices.push(entry->second);
-        entries->emplace_back(FREE, entry->second + 1, 0, 0);
-      } else {
-        entries->emplace_back(FREE, 0, 0, 0);
+        size_t idx = GetIndex(free_indices, max_ptrs);
+        ptr_to_index[entry.ptr] = idx;
+        entries->emplace_back(REALLOC, idx, entry.size, old_pointer_idx + 1);
+        break;
       }
-    } else if (type == "calloc") {
-      // Format:
-      //   TID: calloc POINTER ITEM_SIZE ITEM_COUNT
-      size_t n_elements;
-      size_t size;
-      if (sscanf(line_end, "%zu %zu", &n_elements, &size) != 2) {
-        errx(1, "Internal Error: Failed to read calloc data %s", line.c_str());
-      }
-      size_t idx = GetIndex(free_indices, max_ptrs);
-      indices[pointer] = idx;
-      entries->emplace_back(CALLOC, idx, size, n_elements);
-    } else if (type == "realloc") {
-      // Format:
-      //   TID: calloc POINTER NEW_SIZE OLD_POINTER
-      uintptr_t old_pointer;
-      size_t size;
-      if (sscanf(line_end, "%" SCNxPTR " %zu", &old_pointer, &size) != 2) {
-        errx(1, "Internal Error: Failed to read realloc data %s", line.c_str());
-      }
-      size_t old_pointer_idx = 0;
-      if (old_pointer != 0) {
-        auto entry = indices.find(old_pointer);
-        if (entry == indices.end()) {
-          errx(1, "Internal Error: Failed to find realloc pointer %" PRIuPTR, old_pointer);
+      case FREE:
+        if (entry.ptr != 0) {
+          auto idx_entry = ptr_to_index.find(entry.ptr);
+          if (idx_entry == ptr_to_index.end()) {
+            errx(1, "File Error: Unable to find free pointer %" PRIu64, entry.ptr);
+          }
+          free_indices.push(idx_entry->second);
+          entries->emplace_back(FREE, idx_entry->second + 1, 0, 0);
+        } else {
+          entries->emplace_back(FREE, 0, 0, 0);
         }
-        old_pointer_idx = entry->second;
-        free_indices.push(old_pointer_idx);
-      }
-      size_t idx = GetIndex(free_indices, max_ptrs);
-      indices[pointer] = idx;
-      entries->emplace_back(REALLOC, idx, size, old_pointer_idx + 1);
-    } else if (type == "memalign") {
-      // Format:
-      //   TID: memalign POINTER SIZE ALIGNMENT
-      size_t align;
-      size_t size;
-      if (sscanf(line_end, "%zu %zu", &align, &size) != 2) {
-        errx(1, "Internal Error: Failed to read memalign data %s", line.c_str());
-      }
-      size_t idx = GetIndex(free_indices, max_ptrs);
-      indices[pointer] = idx;
-      entries->emplace_back(MEMALIGN, idx, size, align);
-    } else if (type != "thread_done") {
-      errx(1, "Internal Error: Unknown type %s", line.c_str());
+        break;
+      case THREAD_DONE:
+        // Ignore these.
+        break;
     }
   }
 
@@ -205,21 +150,8 @@ static std::vector<MallocEntry>* GetTraceData(const char* filename, size_t* max_
   return entries;
 }
 
-static __always_inline uint64_t Nanotime() {
-  struct timespec t;
-  t.tv_sec = t.tv_nsec = 0;
-  clock_gettime(CLOCK_MONOTONIC, &t);
-  return static_cast<uint64_t>(t.tv_sec) * 1000000000LL + t.tv_nsec;
-}
-
-static __always_inline void MakeAllocationResident(void* ptr, size_t nbytes, int pagesize) {
-  uint8_t* data = reinterpret_cast<uint8_t*>(ptr);
-  for (size_t i = 0; i < nbytes; i += pagesize) {
-    data[i] = 1;
-  }
-}
-
-static void RunTrace(benchmark::State& state, std::vector<MallocEntry>& entries, size_t max_ptrs) {
+static void RunTrace(benchmark::State& state, std::vector<TraceAllocEntry>& entries,
+                     size_t max_ptrs) {
   std::vector<void*> ptrs(max_ptrs, nullptr);
 
   int pagesize = getpagesize();
@@ -245,7 +177,7 @@ static void RunTrace(benchmark::State& state, std::vector<MallocEntry>& entries,
 
       case CALLOC:
         start_ns = Nanotime();
-        ptr = calloc(entry.arg2, entry.size);
+        ptr = calloc(entry.u.n_elements, entry.size);
         if (ptr == nullptr) {
           errx(1, "calloc returned nullptr");
         }
@@ -260,7 +192,7 @@ static void RunTrace(benchmark::State& state, std::vector<MallocEntry>& entries,
 
       case MEMALIGN:
         start_ns = Nanotime();
-        ptr = memalign(entry.arg2, entry.size);
+        ptr = memalign(entry.u.align, entry.size);
         if (ptr == nullptr) {
           errx(1, "memalign returned nullptr");
         }
@@ -275,11 +207,11 @@ static void RunTrace(benchmark::State& state, std::vector<MallocEntry>& entries,
 
       case REALLOC:
         start_ns = Nanotime();
-        if (entry.arg2 == 0) {
+        if (entry.u.old_idx == 0) {
           ptr = realloc(nullptr, entry.size);
         } else {
-          ptr = realloc(ptrs[entry.arg2 - 1], entry.size);
-          ptrs[entry.arg2 - 1] = nullptr;
+          ptr = realloc(ptrs[entry.u.old_idx - 1], entry.size);
+          ptrs[entry.u.old_idx - 1] = nullptr;
         }
         if (entry.size > 0) {
           if (ptr == nullptr) {
@@ -306,6 +238,9 @@ static void RunTrace(benchmark::State& state, std::vector<MallocEntry>& entries,
         free(ptr);
         total_ns += Nanotime() - start_ns;
         break;
+
+      case THREAD_DONE:
+        break;
     }
   }
   state.SetIterationTime(total_ns / double(1000000000.0));
@@ -319,7 +254,7 @@ static void RunTrace(benchmark::State& state, std::vector<MallocEntry>& entries,
 static void BenchmarkTrace(benchmark::State& state, const char* filename) {
   std::string full_filename(android::base::GetExecutableDirectory() + "/traces/" + filename);
   size_t max_ptrs;
-  std::vector<MallocEntry>* entries = GetTraceData(full_filename.c_str(), &max_ptrs);
+  std::vector<TraceAllocEntry>* entries = GetTraceData(full_filename.c_str(), &max_ptrs);
   if (entries == nullptr) {
     errx(1, "ERROR: Failed to get trace data for %s.", full_filename.c_str());
   }