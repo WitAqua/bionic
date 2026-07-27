jbeich/platform_system_extras

[GWP-ASan] Scan GWP-ASan regions in maps.
GWP-ASan is part of the native allocator, and may allocate some memory.
If a trace comes from a GWP-ASan sampled process, we need to identify
GWP-ASan allocations as from the native heap.

Bug: 135634846
Test: atest memory_replay_tests
Change-Id: Icd1122130a30af5604554a62d270dc1967117570

memory_replay/tests/NativeInfoTest.cpp
@@ -269,7 +269,55 @@ TEST_F(NativeInfoTest, mix_heap_anon) {
       "KernelPageSize:        4 kB\n"
       "MMUPageSize:           4 kB\n"
       "Locked:                0 kB\n"
-      "Name:           [anon:scudo:test]\n";
+      "Name:           [anon:scudo:test]\n"
+      "b8f4e000-b8f6f000 rw-p 00000000 00:00 0          [anon:GWP-ASan Alive Slot]\n"
+      "Size:                  4 kB\n"
+      "Rss:                   4 kB\n"
+      "Pss:                   0 kB\n"
+      "Shared_Clean:          0 kB\n"
+      "Shared_Dirty:          0 kB\n"
+      "Private_Clean:         0 kB\n"
+      "Private_Dirty:         0 kB\n"
+      "Referenced:            0 kB\n"
+      "Anonymous:             0 kB\n"
+      "AnonHugePages:         0 kB\n"
+      "Swap:                  0 kB\n"
+      "KernelPageSize:        4 kB\n"
+      "MMUPageSize:           4 kB\n"
+      "Locked:                0 kB\n"
+      "Name:           [anon:GWP-ASan Alive Slot]\n"
+      "b8f4c000-b8f6d000 rw-p 00000000 00:00 0          [anon:GWP-ASan Alive Slot]\n"
+      "Size:                  4 kB\n"
+      "Rss:                   4 kB\n"
+      "Pss:                   0 kB\n"
+      "Shared_Clean:          0 kB\n"
+      "Shared_Dirty:          0 kB\n"
+      "Private_Clean:         0 kB\n"
+      "Private_Dirty:         0 kB\n"
+      "Referenced:            0 kB\n"
+      "Anonymous:             0 kB\n"
+      "AnonHugePages:         0 kB\n"
+      "Swap:                  0 kB\n"
+      "KernelPageSize:        4 kB\n"
+      "MMUPageSize:           4 kB\n"
+      "Locked:                0 kB\n"
+      "Name:           [anon:GWP-ASan Alive Slot]\n"
+      "b9f4d000-b8f6e000 rw-p 00000000 00:00 0          [anon:GWP-ASan Guard Page]\n"
+      "Size:                  4 kB\n"
+      "Rss:                   0 kB\n"
+      "Pss:                   0 kB\n"
+      "Shared_Clean:          0 kB\n"
+      "Shared_Dirty:          0 kB\n"
+      "Private_Clean:         0 kB\n"
+      "Private_Dirty:         0 kB\n"
+      "Referenced:            0 kB\n"
+      "Anonymous:             0 kB\n"
+      "AnonHugePages:         0 kB\n"
+      "Swap:                  0 kB\n"
+      "KernelPageSize:        4 kB\n"
+      "MMUPageSize:           4 kB\n"
+      "Locked:                0 kB\n"
+      "Name:           [anon:GWP-ASan Guard Page]\n";
 
   ASSERT_TRUE(TEMP_FAILURE_RETRY(
       write(tmp_file_->fd, smaps_data.c_str(), smaps_data.size())) != -1);
@@ -278,6 +326,6 @@ TEST_F(NativeInfoTest, mix_heap_anon) {
   size_t rss_bytes = 1;
   size_t va_bytes = 1;
   NativeGetInfo(tmp_file_->fd, &rss_bytes, &va_bytes);
-  EXPECT_EQ(126976U, rss_bytes);
-  EXPECT_EQ(147456U, va_bytes);
+  EXPECT_EQ(135168U, rss_bytes);
+  EXPECT_EQ(159744U, va_bytes);
 }

WitAqua/bionic

memory/replay/tests/NativeInfoTest.cpp
@@ -269,7 +269,55 @@ TEST_F(NativeInfoTest, mix_heap_anon) {
       "KernelPageSize:        4 kB\n"
       "MMUPageSize:           4 kB\n"
       "Locked:                0 kB\n"
-      "Name:           [anon:scudo:test]\n";
+      "Name:           [anon:scudo:test]\n"
+      "b8f4c000-b8f4d000 rw-p 00000000 00:00 0          [anon:GWP-ASan Guard Page]\n"
+      "Size:                  4 kB\n"
+      "Rss:                   0 kB\n"
+      "Pss:                   0 kB\n"
+      "Shared_Clean:          0 kB\n"
+      "Shared_Dirty:          0 kB\n"
+      "Private_Clean:         0 kB\n"
+      "Private_Dirty:         0 kB\n"
+      "Referenced:            0 kB\n"
+      "Anonymous:             0 kB\n"
+      "AnonHugePages:         0 kB\n"
+      "Swap:                  0 kB\n"
+      "KernelPageSize:        4 kB\n"
+      "MMUPageSize:           4 kB\n"
+      "Locked:                0 kB\n"
+      "Name:           [anon:GWP-ASan Guard Page]\n"
+      "b8f4d000-b8f4e000 rw-p 00000000 00:00 0          [anon:GWP-ASan Alive Slot]\n"
+      "Size:                  4 kB\n"
+      "Rss:                   4 kB\n"
+      "Pss:                   0 kB\n"
+      "Shared_Clean:          0 kB\n"
+      "Shared_Dirty:          0 kB\n"
+      "Private_Clean:         0 kB\n"
+      "Private_Dirty:         0 kB\n"
+      "Referenced:            0 kB\n"
+      "Anonymous:             0 kB\n"
+      "AnonHugePages:         0 kB\n"
+      "Swap:                  0 kB\n"
+      "KernelPageSize:        4 kB\n"
+      "MMUPageSize:           4 kB\n"
+      "Locked:                0 kB\n"
+      "Name:           [anon:GWP-ASan Alive Slot]\n"
+      "b8f4e000-b8f4f000 rw-p 00000000 00:00 0          [anon:GWP-ASan Guard Page]\n"
+      "Size:                  4 kB\n"
+      "Rss:                   0 kB\n"
+      "Pss:                   0 kB\n"
+      "Shared_Clean:          0 kB\n"
+      "Shared_Dirty:          0 kB\n"
+      "Private_Clean:         0 kB\n"
+      "Private_Dirty:         0 kB\n"
+      "Referenced:            0 kB\n"
+      "Anonymous:             0 kB\n"
+      "AnonHugePages:         0 kB\n"
+      "Swap:                  0 kB\n"
+      "KernelPageSize:        4 kB\n"
+      "MMUPageSize:           4 kB\n"
+      "Locked:                0 kB\n"
+      "Name:           [anon:GWP-ASan Guard Page]\n";
 
   ASSERT_TRUE(TEMP_FAILURE_RETRY(
       write(tmp_file_->fd, smaps_data.c_str(), smaps_data.size())) != -1);
@@ -278,6 +326,6 @@ TEST_F(NativeInfoTest, mix_heap_anon) {
   size_t rss_bytes = 1;
   size_t va_bytes = 1;
   NativeGetInfo(tmp_file_->fd, &rss_bytes, &va_bytes);
-  EXPECT_EQ(126976U, rss_bytes);
-  EXPECT_EQ(147456U, va_bytes);
+  EXPECT_EQ(131072U, rss_bytes);
+  EXPECT_EQ(159744U, va_bytes);
 }